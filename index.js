f775ff08350e582c5b619ca0d7a3e2a4dd98cbac
hani1415 <46425434+hani1415@users.noreply.github.com>
2019-01-21 18:03:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-﻿const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
-  client.user.setGame(`BlackTwo.`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
+  console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
 client.on('message', message => {
+    var prefix = '!'
+    
    let embed = new Discord.RichEmbed()
 
     let args = message.content.split(' ').slice(1).join(' ');
      if(!message.channel.guild) return;
-if(message.content.split(' ')[0] == 'Bbc') {
+if(message.content.split(' ')[0] == prefix + 'bc') {
          message.react("✔️")
           let embed = new Discord.RichEmbed()
-    .setColor("#05a8f5")  
-                                      .addField('تم الإرسال بنجاح :ballot_box_with_check:',"سهل")
-									message.delete();
-		message.channel.sendEmbed(embed);
+    .setColor("#FF00FF")
+    .setThumbnail(message.author.avatarURL)   
+                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
+                 message.channel.sendEmbed(embed);
         message.guild.members.forEach(m => {
             var bc = new Discord.RichEmbed()
 .addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
             .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
-    .setColor('##05a8f5')
+    .setColor('#ff0000')
                  .addField('ّ', args)
             m.send(``,{embed: bc});
         });
     }
 })
-
 
 
 client.login(process.env.BOT_TOKEN);
